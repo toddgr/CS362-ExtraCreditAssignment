973680a97447202b0b9b0c7b2778ef96bb93e3ed
Grace Todd <toddgr@oregonstate.edu>
2021-06-06 20:06:17 -0700

    add pytests for question 3

--- test_questions.py
+++ test_questions.py
@@ -25,3 +25,11 @@ def test_question1_3():
 
 def test_question3_1():
     assert [4, 8] == Question3.findSum([1, 2, 3, 4, 5, 6, 7, 8], 12)
+
+
+def test_question3_2():
+    assert [15,13] == Question3.findSum([2, 3, 9, 15, 24, 6, 13, 20], 28)
+
+
+def test_question3_3():
+    assert [10, 5] == Question3.findSum([9, 18, 10, 24, 19, 15], 15)

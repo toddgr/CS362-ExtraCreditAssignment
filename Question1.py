5d7eba719104b0820eda730438deb388a1de99a8
Grace Todd <toddgr@oregonstate.edu>
2021-06-06 17:55:23 -0700

    added test for Question 1

---
+++ Question1.py
@@ -0,0 +1,12 @@
+"""
+Grace Todd
+CS 362: Software Engineering II
+Spring 2021
+
+EXTRA CREDIT ASSIGNMENT QUESTION 1
+Write a program that reverses a sentence.
+Input - ask the user for a sentence: a long string
+containing multiple words. Return to the user
+the same string,  with the words in backward order.
+"""
+
--- test.py
+++ test.py
@@ -7,9 +7,14 @@ Test file for Extra Credit assignment
 """
 
 
-#import Question1
-#import unittest
+import Question1
+import unittest
 
 
-#if __name__ == "__main__":
-#    unittest.main()
+class TestCase(unittest.TestCase):
+
+    def test_question1_1(self):
+        self.assertEqual(Question1.reverseSentence("My name is Grace"), "Grace is name My")
+
+if __name__ == "__main__":
+    unittest.main()

e332906dd63d51b3c273836c6077fe5fad42f6b7
Grace Todd <toddgr@oregonstate.edu>
2021-06-06 18:17:23 -0700

    add Question 2 code file

---
+++ Question2.py
@@ -0,0 +1,13 @@
+"""
+Grace Todd
+CS 362: Software Engineering II
+Spring 2021
+EXTRA CREDIT ASSIGNMENT QUESTION 2
+Write 5 user stories for your social
+media system from in-class activity 1.
+Your user stories should have the following
+format - As - I need/I want - so that.
+They should have a success criterion.
+Follow examples in lecture documents and resources. 
+
+"""

4ac5108a7404bcf2285feafaeb8f2065d728d300
Grace Todd <toddgr@oregonstate.edu>
2021-06-06 19:55:47 -0700

    add unittests for question 1

--- test.py
+++ test.py
@@ -19,6 +19,16 @@ class TestCase(unittest.TestCase):
         result = "Grace is name My"
         self.assertEqual(Question1.reverseSentence(sentence), result)
 
+    def test_question1_2(self):
+        sentence = "This is for the extra credit assignment."
+        result = "assignment credit extra the for is This"
+        self.assertEqual(Question1.reverseSentence(sentence), result)
+
+    def test_question1_3(self):
+        sentence = "I like to eat ice cream"
+        result = "cream ice eat to like I"
+        self.assertEqual(Question1.reverseSentence(sentence), result)
+
     def test_question3_1(self):
         array = [2, 8, 9, 12, 14, 67]
         target_sum = 21

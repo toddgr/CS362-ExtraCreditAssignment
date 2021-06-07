8ab072912783bb2a6e7bb199f50ccfb3195c8a2f
Grace Todd <toddgr@oregonstate.edu>
2021-06-06 17:48:35 -0700

    added test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+"""
+Grace Todd
+CS 362: Software Engineering II
+Spring 2021    
+
+Test file for Extra Credit assignment
+"""
+
+
+#import Question1
+#import unittest
+
+
+#if __name__ == "__main__":
+#    unittest.main()
